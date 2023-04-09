8108c26bf4d417e695ed87d2719ce6d913e2a5af
flavio.alibeaj <flavio.alibeaj@gmail.com>
2023-04-09 18:27:06 +0200

    Built-in Directives

--- hotelInventoryApp/src/app/rooms/rooms.component.ts
+++ hotelInventoryApp/src/app/rooms/rooms.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Room, RoomList } from './rooms';
 
 @Component({
   selector: 'app-rooms',
@@ -7,8 +8,47 @@ import { Component } from '@angular/core';
 })
 export class RoomsComponent {
   hotelName: string = "Onyx Hotel"
+
   numberOfRooms: number = 10
+
   hideRooms: boolean = false
+
+  room: Room = {
+    totalRooms: 20,
+    availableRooms: 10,
+    bookedRooms: 5
+  }
+
+  roomList: RoomList[] = [
+    {
+      roomNumber: 91,
+      roomType: "Deluxe Room",
+      amenities: "AC, Free Wi-fi, Bathroom, TV, Kitchen",
+      price: 200,
+      photos: "https://images.unsplash.com/photo-1591088398332-8a7791972843?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+      checkInTime: new Date("08-04-2023"),
+      checkOutTime: new Date("11-04-2023")
+    },
+    {
+      roomNumber: 32,
+      roomType: "Deluxe Room",
+      amenities: "AC, Free Wi-fi, Bathroom, TV, Kitchen",
+      price: 500,
+      photos: "https://images.unsplash.com/photo-1591088398332-8a7791972843?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+      checkInTime: new Date("11-06-2023"),
+      checkOutTime: new Date("14-06-2023")
+    },
+    {
+      roomNumber: 311,
+      roomType: "Private Suite",
+      amenities: "AC, Free Wi-fi, Bathroom, TV, Kitchen",
+      price: 1000,
+      photos: "https://images.unsplash.com/photo-1591088398332-8a7791972843?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+      checkInTime: new Date("28-09-2023"),
+      checkOutTime: new Date("30-09-2023")
+    }
+  ]
+
   toggle = (): void => {
     this.hideRooms = !this.hideRooms
   }
---
+++ hotelInventoryApp/src/app/rooms/rooms.ts
@@ -0,0 +1,17 @@
+import { NumericLiteral } from "typescript"
+
+export interface Room {
+    totalRooms: number
+    availableRooms: number
+    bookedRooms: number
+}
+
+export interface RoomList {
+    roomNumber: number
+    roomType: string
+    amenities: string
+    price: number
+    photos: string
+    checkInTime: Date
+    checkOutTime: Date
+}
